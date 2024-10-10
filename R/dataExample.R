4ae4597b588bbe8b0e1be1266fe8deb077678f96
Jedidiah Harwood <jedharwood@Jedidiahs-Mac-mini.local>
2024-10-10 15:34:50 -0700

    added stock data

--- R/dataExample.R
+++ R/dataExample.R
@@ -1,20 +1,17 @@
-#' Stock data
+#' Example dataset: stocks
 #' 
-#' @name stocks
+#' @format A 283 by 1007 matrix
 #' 
 #' @description
-#' A 283 by 1007 matrix with Stock Data from the S&P 500.
+#' A matrix with Stock Data from the S&P 500.
 #' 
 #' @docType data
 #' 
 #' @usage data(stocks) 
 #' 
-#' @format An object of class `matrix`
-#' 
-#' @keywords dataset
-#' 
 #' @references Peng et al. 2024
 #' 
 #' @examples
 #' data(stocks)
-NULL
+#' @source TBD
+"stocks"
