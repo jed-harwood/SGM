e8173068dbfa50b61e2f0d63867581fd0fd189a7
Jedidiah Harwood <jedharwood@Jedidiahs-Mac-mini.local>
2024-10-17 21:35:02 -0700

    Added information on stock data

--- R/dataExample.R
+++ R/dataExample.R
@@ -3,7 +3,7 @@
 #' @format A 283 by 1007 matrix
 #' 
 #' @description
-#' A matrix with Stock Data from the S&P 500.
+#' A matrix with information on 283 stocks across five GICS sectors: 58 from Information Technology, 72 from Consumer Discretionary, 32 from Consumer Staples, 59 from Financials, and 62 from Industrials. The dataset spans January 1, 2007, to January 1, 2011, covering the global financial crisis, with 1007 closing prices per stock.
 #' 
 #' @docType data
 #' 
@@ -13,5 +13,6 @@
 #' 
 #' @examples
 #' data(stocks)
-#' @source TBD
+#' @source Ryan JA, Ulrich JM (2024). quantmod: Quantitative Financial Modelling Framework. R package version 0.4.26, <https://CRAN.R-project.org/package=quantmod>.
+
 "stocks"
